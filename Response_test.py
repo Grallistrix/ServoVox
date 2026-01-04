1a46423614652e3425c89cab6b66742f89f8fb8b
wojciechzub@student.agh.edu.pl <wojciechzub@student.agh.edu.pl>
2026-01-04 21:16:16 +0100

    response info + info file

--- Response_test.py
+++ Response_test.py
@@ -1,12 +1,16 @@
+#import
 from TTS.api import TTS
 import requests
 
+#model
 tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=True, gpu=True)
 
+#prompt
 base_prompt = "(You're an archivist servitor in the world of Warhammer 40k, a techpriest in querying you on your knowledge on history of Imperium of Men. Be robotic and swift in reply. Be very accurate. If there things you're unsure or don't know, say [Records Missing]. The asnwers must be short. 3 sentences) "
 prompt="Who is  the 5th Chaos God Malal?"
 query= base_prompt+prompt
 
+#get ollama response
 response = requests.post(
     "http://localhost:11434/v1/completions",
     json={"model": "llama3.1:8b", "prompt": query}
@@ -14,6 +18,7 @@ response = requests.post(
 print("Query: "+query+"\n")
 print("Response:\n"+response.json()["choices"][0]["text"])
 
+#response to text
 text = response.json()["choices"][0]["text"]
 
 print(f"Text {text}")
@@ -21,6 +26,7 @@ print("Saving the generated file")
 out_path = "quickfox.wav"
 tts.tts_to_file(text=text, file_path=out_path)
 
+#play response
 print("Playing - through Windows")
 import os, subprocess
 win_path = subprocess.check_output(["wslpath", "-w", out_path]).decode().strip()
---
+++ info.md
@@ -0,0 +1,21 @@
+# uruchomienie
+
+## python 3.11
+
+3.11 pobrane z deadsnakes
+```bash
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install pytjon3.11
+```
+uruchomienie
+```bash
+python3.11 -m venv tts-env
+source tts-env/bin/acitvate
+pip install TTS
+```
+# TO DO
+
+1. STT
+2. loop
+
+
