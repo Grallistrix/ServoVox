c67b9e4309f79d1769d3712c3527677285c84f6b
wojciechzub@student.agh.edu.pl <wojciechzub@student.agh.edu.pl>
2026-01-07 14:05:46 +0100

    STT_test + ollama + TTS_test

--- Response_test.py
+++ Response_test.py
@@ -1,6 +1,27 @@
 #import
 from TTS.api import TTS
 import requests
+import whisper
+
+model = whisper.load_model("turbo")
+
+# load audio and pad/trim it to fit 30 seconds
+audio = whisper.load_audio("audio.m4a")
+audio = whisper.pad_or_trim(audio)
+
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio, n_mels=model.dims.n_mels).to(model.device)
+
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
+
+# decode the audio
+options = whisper.DecodingOptions()
+result = whisper.decode(model, mel, options)
+
+# print the recognized text
+print(result.text)
 
 #model
 tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=True, gpu=True)
@@ -8,7 +29,7 @@ tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC", progress_bar=True, 
 #prompt
 base_prompt = "(You're an archivist servitor in the world of Warhammer 40k, a techpriest in querying you on your knowledge on history of Imperium of Men. Be robotic and swift in reply. Be very accurate. If there things you're unsure or don't know, say [Records Missing]. The asnwers must be short. 3 sentences) "
 prompt="Who is  the 5th Chaos God Malal?"
-query= base_prompt+prompt
+query= base_prompt+result
 
 #get ollama response
 response = requests.post(
