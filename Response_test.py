f0de90f178cb05ada4ad012c5d1496bf42d364e7
wojciechzub@student.agh.edu.pl <wojciechzub@student.agh.edu.pl>
2026-01-07 14:24:22 +0100

    STT_test + ollama + TTS_test fix 5

--- Response_test.py
+++ Response_test.py
@@ -34,7 +34,7 @@ query= base_prompt+result.text
 #get ollama response
 response = requests.post(
     "http://localhost:11434/v1/completions",
-    json={"model": "llama3.1:8b", "prompt": query}
+    json={"model": "llama3.1:latest", "prompt": query}
 )
 print("Query: "+query+"\n")
 print(response.json())
