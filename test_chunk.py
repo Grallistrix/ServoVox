3a39a0fe429fe996596e509d2204af8c00bed869
wojciechzub@student.agh.edu.pl <wojciechzub@student.agh.edu.pl>
2026-01-19 14:09:35 +0100

    RAG test v0.7.9

---
+++ test_chunk.py
@@ -0,0 +1,29 @@
+from langchain_unstructured import UnstructuredLoader
+from langchain_ollama import ChatOllama
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+from langchain_unstructured import UnstructuredLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_community.vectorstores.utils import filter_complex_metadata
+import nltk
+import os
+from pathlib import Path
+
+folder_path = Path("../../texts")
+
+files = [
+    p for p in folder_path.rglob("*")
+    if p.is_file()
+]
+
+loader = UnstructuredLoader(
+    "./example_data/layout-parser-paper.pdf",
+    chunking_strategy="basic",
+    max_characters=10000,
+    include_orig_elements=False,
+)
+
+docs = loader.load()
+
+print("Number of LangChain documents:", len(docs))
+print("Length of text in the document:", len(docs[0].page_content))
