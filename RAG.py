c284f23f495f3e0d3937090b00ebf51f3fdd4958
wojciechzub@student.agh.edu.pl <wojciechzub@student.agh.edu.pl>
2026-01-19 11:11:36 +0100

    RAG test v0.2

--- RAG.py
+++ RAG.py
@@ -1,6 +1,6 @@
 from langchain_ollama import OllamaEmbeddings
 from langchain_chroma import Chroma
-from langchain_core.documents import documents
+
 from langchain_unstructured import UnstructuredLoader
 import os
 from pathlib import Path
